74247652c43186765d5fc78a115efe1a94398cd0
simpart <simpart>
2019-07-29 20:50:38 +0900

    mod menu

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
  * @brief slide menu component for mofron
  * @author simpart
  */
-const mf = require('mofron');
-const Focus = require('mofron-event-clkfocus');
-const Position = require('mofron-effect-position');
-const Border = require('mofron-effect-border');
-const HrzPos = require('mofron-effect-hrzpos');
-const Click = require('mofron-event-click');
-const Menu = require('mofron-comp-menu');
-const Text = require('mofron-comp-text');
+const mf = require("mofron");
+const Focus = require("mofron-event-clkfocus");
+const Position = require("mofron-effect-position");
+const Border = require("mofron-effect-border");
+const HrzPos = require("mofron-effect-hrzpos");
+const SynWid = require("mofron-effect-syncwid");
+const Click = require("mofron-event-click");
+const Menu = require("mofron-comp-menu");
+const Text = require("mofron-comp-text");
 
-let sm_open   = new Position({ eid: 0, tag: "SlideMenu-open"  });
-let sm_close  = new Position({ eid: 1, tag: "SlideMenu-close" });
-let sm_border = new Border({ tag: "SlideMenu", color: [190,190,190] });
+
+let sm_open   = new Position({ eid: 0, direction: "left", tag: "SlideMenu-open"  });
+let sm_close  = new Position({ eid: 1, direction: "left", tag: "SlideMenu-close" });
+let sm_border = new Border({ type: "right", tag: "SlideMenu", color: [190,190,190] });
 
 mofron.comp.SlideMenu = class extends mf.Component {
     /**
@@ -44,14 +46,45 @@ mofron.comp.SlideMenu = class extends mf.Component {
             this.style({ "display" : "flex" });
             
             /* set default switch */
-            this.switch(
-                new Text({ text: '&equiv;', size: '0.4rem' })
-            );
-            this.child([this.switch(), this.menu()]);
+            this.switch(new Text({ text: '&equiv;', size: '0.4rem' }));
+            
+            let sd   = this;
+            let mwrp = new mf.Component({
+                style: { "position":"fixed", "top" : "0rem" },
+                effect: [
+                    sm_border, sm_open, sm_close,
+                    new SynWid({ targetComp: this.menu(), tag: "SlideMenu" })
+                ],
+                event: [
+                    new Click({
+                       handler: () => {
+                           try {
+                               sd.slidemng(true);
+                               let fcs = sd.switch().event("ClkFocus").clickFlag();
+                               if (false === fcs) {
+                                   sd.switch().event("ClkFocus").clickFlag(true);
+                               } else {
+                                   sd.slidemng(null);
+                               }
+                           } catch (e) {
+                               console.error(e.stack);
+                               throw e;
+                           }
+                       },
+                       pointer: false
+                    })
+                ],
+                height: "100%", child: this.menu(), visible: false, 
+            });
+            
+            this.child([this.switch().parent(), mwrp]);
             
             this.target(this.menu().target());
             this.styleTgt(this.target());
             this.eventTgt(this.target());
+            
+            this.width("2rem");
+            this.baseColor([255,255,255]);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -66,7 +99,8 @@ mofron.comp.SlideMenu = class extends mf.Component {
     beforeRender () {
         try {
             super.beforeRender();
-            this.style({ "height" : "100%" });
+            sm_open.value("-" + this.width(), "0rem");
+            sm_close.value("0rem", "-" + this.width());
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -83,6 +117,9 @@ mofron.comp.SlideMenu = class extends mf.Component {
      */
     switch (prm) {
         try {
+            if (undefined === prm) {
+                return this.innerComp("switch").child()[0];
+            }
             if (true === mf.func.isComp(prm)) {
                 let sd = this;
                 prm.event(new Focus(
@@ -102,7 +139,7 @@ mofron.comp.SlideMenu = class extends mf.Component {
                           style:{ "display":"flex" }, child: prm
                       });
             }
-            return this.innerComp("switch", prm, mf.Component);
+            this.innerComp("switch", prm, mf.Component);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -120,65 +157,30 @@ mofron.comp.SlideMenu = class extends mf.Component {
         try {
             if (true === mf.func.isComp(prm, "Menu")) {
                 /* set effect option */
-                sm_open.direction(this.position());
-                sm_close.direction(this.position());
                 sm_border.type(("left" === this.position()) ? "right" : "left");
+                let sd = this;
                 prm.option({
-                    effect: [ sm_border, sm_open, sm_close ],
-                    style: { "position":"fixed", "top" : "0rem" },
-                    visible: false
+                    selectEvent: () => {
+                        setTimeout(
+                            () => {
+                                sd.slidemng(false);
+                                sd.switch().event("ClkFocus").focusSts(false);
+                            },
+                            80
+                        );
+                    },
                 });
-                /* set color */
+                
                 if (undefined !== this.innerComp()["menu"]) {
+                    /* it is overwrite menu */
+                    /* set color */
                     prm.option({
                        baseColor: this.mainColor(),
                        accentColor: this.accentColor(),
                     });
-                    let bs_clr = this.baseColor();
-                    if (undefined !== bs_clr) {
-                        prm.style({ background: bs_clr[0] }, bs_clr[1]);
-                    }
-                }
-                /* set slide width */
-                let wid = (null !== prm.width()) ? prm.width() : this.width();
-                if (null === wid) {
-                    wid = "2rem";
-                }
-                sm_open.value("-" + wid, "0rem");
-                sm_close.value("0rem", "-" + wid);
-                
-                /* set slide action */
-                let sd = this;
-                prm.event(
-                    new Click({
-                        handler: () => {
-                            sd.slidemng(true);
-                            let fcs = sd.switch().child()[0].event("ClkFocus").clickFlag();
-                            if (false === fcs) {
-                                sd.switch().child()[0].event("ClkFocus").clickFlag(true);
-                            } else {
-                                sd.slidemng(null);
-                            }
-                        },
-                        pointer: false
-                    })
-                );
-                prm.selectEvent(() => {
-                    setTimeout(
-                        () => {
-                            sd.slidemng(false);
-                            sd.switch().child()[0].event("ClkFocus").focusSts(false);
-                        },
-                        80
-                    );
-                });
-                /* set default background */
-                prm.style(
-                    { "background": "rgb(255,255,255)" },
-                    { loose:true }
-                );
-                /* replace target */
-                if (undefined !== this.innerComp()["menu"]){
+                    /* replace sync target */
+                    this.menu().parent().effect(["SyncWid", "SlideMenu"]).targetComp(prm);
+                    /* replace target */
                     this.target(prm.target());
                     this.styleTgt(prm.target());
                     this.eventTgt(prm.target());
@@ -213,8 +215,8 @@ mofron.comp.SlideMenu = class extends mf.Component {
     slidemng (prm) {
         try {
             if ( ('boolean' === typeof prm) && (null !== this.slidemng()) ) {
-                if ( (false === prm) || (prm !== this.menu().visible())) {
-                    this.menu().visible(prm);
+                if ( (false === prm) || (prm !== this.menu().parent().visible())) {
+                    this.menu().parent().visible(prm);
                 }
             }
             return this.member("slidemng", "boolean", prm, false);
@@ -235,7 +237,7 @@ mofron.comp.SlideMenu = class extends mf.Component {
         try {
             if (undefined !== prm) {
                 /* set switch position */
-                this.switch().effect(["HrzPos","SlideMenu"]).type(prm);
+                this.switch().parent().effect(["HrzPos","SlideMenu"]).type(prm);
                 /* set menu position */
                 sm_open.direction(prm);
                 sm_close.direction(prm);
@@ -275,11 +277,7 @@ mofron.comp.SlideMenu = class extends mf.Component {
      */
     baseColor (prm) {
         try {
-            if (undefined === prm) {
-                return this.m_bsclr;
-            }
-            this.m_bsclr = ("string" === typeof prm) ? [prm, undefined] : prm;
-            this.menu().tgtColor('background', this.m_bsclr);
+            return mf.func.cmpColor(this.menu().parent(), "background", prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
